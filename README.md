bcc41e29c39d0a8169ec042e013c453547515e5a
Anders Austad <anders.austad@novanet.no>
2020-02-17 13:24:35 +0100

    Add luke02 implementation

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 My C# solutions to knowit's [2019 Christmas calendar](https://julekalender.knowit.no/) challenges
 
-| Challenge | My solution | Discussion |
-| ------------- | ------------- | ------------- |
-| [Luke 01](https://julekalender.knowit.no/doors/ck3ln1prramud0109sqetdk7g) | | [Discussion](https://gist.github.com/knowitkodekalender/1f3b32afaa41d5cc996623680de02131) |
+| Day | Challenge | My solution | Discussion |
+| ------------- | ------------- | ------------- | ------------- |
+| 01 | [Drageproblemer](https://julekalender.knowit.no/doors/ck3ln1prramud0109sqetdk7g) | [program.cs](https://github.com/anderaus/knowit-julekalender-2019/blob/master/luke01/Program.cs) | [Discussion](https://gist.github.com/knowitkodekalender/1f3b32afaa41d5cc996623680de02131) |
+| 02 | [Syndefloden](https://julekalender.knowit.no/doors/ck3n9yxzraz7h0109wu72kzoo) | [program.cs](https://github.com/anderaus/knowit-julekalender-2019/blob/master/luke02/Program.cs) | [Discussion](https://gist.github.com/knowitkodekalender/a8677568f08a4ad723930f0cc047aa77) |
---
+++ luke02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // var worldLines = File.ReadAllLines("world_test.txt");
+        var worldLines = File.ReadAllLines("world.txt");
+
+        double potentialReservoirSize = 0;
+        bool previousPointWasEarth = false;
+        double lineSum = 0;
+        double totalSum = 0;
+
+        foreach (var line in worldLines)
+        {
+            lineSum = 0;
+            previousPointWasEarth = false;
+            potentialReservoirSize = 0;
+
+            foreach (char point in line)
+            {
+                if (point == ' ')
+                {
+                    if (previousPointWasEarth)
+                    {
+                        potentialReservoirSize = 1;
+                    }
+                    else if (potentialReservoirSize > 0)
+                    {
+                        potentialReservoirSize++;
+                    }
+
+                    // Set state for next round
+                    previousPointWasEarth = false;
+                }
+                if (point == '#')
+                {
+                    // Found complete reservoir, sum it up!
+                    if (potentialReservoirSize > 0)
+                    {
+                        lineSum += potentialReservoirSize;
+                        potentialReservoirSize = 0;
+                    }
+
+                    // Set state for next round
+                    previousPointWasEarth = true;
+                }
+            }
+            Console.Write($"     LINESUM: {lineSum}");
+            totalSum += lineSum;
+            Console.WriteLine();
+        }
+        Console.WriteLine($"TOTAL SUM: {totalSum}");
+    }
+}
