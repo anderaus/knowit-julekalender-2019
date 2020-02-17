3ef0fa78b8f4cc0d702ff40c2a26e2b978418b0a
Anders Austad <anders.austad@novanet.no>
2020-02-17 13:05:23 +0100

    Add luke01 implementation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# knowit-julekalender-2019
-My C# solutions to knowit's 2019 Christmas calendar challenges
+# knowit julekalender 2019
+
+My C# solutions to knowit's [2019 Christmas calendar](https://julekalender.knowit.no/) challenges
+
+| Challenge | My solution | Discussion |
+| ------------- | ------------- | ------------- |
+| [Luke 01](https://julekalender.knowit.no/doors/ck3ln1prramud0109sqetdk7g) | | [Discussion](https://gist.github.com/knowitkodekalender/1f3b32afaa41d5cc996623680de02131) |
---
+++ luke01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dragonSize = 50;
+        int numberOfSheep = 0;
+        int day = 1;
+        int sequentialUnderfedDays = 0;
+
+        var sheepAddedPerDay = File.ReadAllText("sau.txt").Split(',').Select(int.Parse);
+
+        foreach (var addedSheep in sheepAddedPerDay)
+        {
+            numberOfSheep += addedSheep;
+            Console.Write($"Day #{day.ToString("000")}. Dragonsize: {dragonSize.ToString("000")}. SheepAtStartOfDay: {numberOfSheep.ToString("000")} ");
+
+            if (dragonSize <= numberOfSheep)
+            {
+                numberOfSheep -= dragonSize;
+                dragonSize++;
+                sequentialUnderfedDays = 0;
+            }
+            else
+            {
+                numberOfSheep = 0;
+                dragonSize--;
+                sequentialUnderfedDays++;
+
+                Console.Write($"Underfed! {sequentialUnderfedDays} times in a row");
+                if (sequentialUnderfedDays >= 5)
+                {
+                    Console.Write($"\nTHE DRAGON WENT BERZERK ON DAY #{day.ToString("000")}. THE ANSWER IS {day - 1}");
+                    return;
+                }
+            }
+
+            day++;
+            Console.WriteLine();
+        }
+    }
+}
