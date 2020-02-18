0b92713f1fc2d409b0ca71415628f8fa5b452904
Anders Austad <anderaus@gmail.com>
2020-02-18 18:41:52 +0100

    Add luke06 implementation

---
+++ luke06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var image = Image.Load("mush.png").CloneAs<Rgba32>())
+        {
+            var span = image.GetPixelSpan();
+
+            for (var i = span.Length - 1; i > 0; i--)
+            {
+                span[i].R = (byte)(span[i].R ^ span[i - 1].R);
+                span[i].G = (byte)(span[i].G ^ span[i - 1].G);
+                span[i].B = (byte)(span[i].B ^ span[i - 1].B);
+            }
+
+            image.Save("solution.png");
+            Console.WriteLine("XOR'ed image saved");
+        }
+    }
+}
