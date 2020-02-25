d3699ef19d88957360a41da183484fb12cbe3d1e
Anders Austad <anders.austad@novanet.no>
2020-02-25 13:42:20 +0100

    Add luke11 implementation

---
+++ luke11/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // EXAMPLE
+        // var stripe = "IIGGFFAIISGIFFSGFFAAASS";
+        // double speed = 300;
+
+        // REAL
+        var stripe = File.ReadAllText("terreng.txt");
+        double speed = 10703437;
+
+        var distance = 0;
+        var iceInARow = 0;
+        var previousWasMountain = false;
+
+        for (int i = 0; i < stripe.Length; i++)
+        {
+            var terreng = stripe[i];
+
+            if (terreng == 'G')
+            {
+                speed -= 27;
+                previousWasMountain = false;
+                iceInARow = 0;
+            }
+            else if (terreng == 'I')
+            {
+                iceInARow++;
+                speed += 12 * iceInARow;
+                previousWasMountain = false;
+            }
+            else if (terreng == 'A')
+            {
+                speed -= 59;
+                previousWasMountain = false;
+                iceInARow = 0;
+            }
+            else if (terreng == 'S')
+            {
+                speed -= 212;
+                previousWasMountain = false;
+                iceInARow = 0;
+            }
+            else if (terreng == 'F')
+            {
+                if (previousWasMountain)
+                {
+                    speed += 35;
+                    previousWasMountain = false;
+                }
+                else
+                {
+                    speed -= 70;
+                    previousWasMountain = true;
+                }
+                iceInARow = 0;
+            }
+
+            distance++;
+
+            if (speed <= 0)
+            {
+                Console.WriteLine($"\nSleden stoppet!\nSluttdistanse: {distance} km");
+                break;
+            }
+        }
+    }
+}
