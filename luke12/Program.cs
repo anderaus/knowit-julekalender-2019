1b900269ee944f83b80e186077434bc298111a7c
Anders Austad <anders.austad@novanet.no>
2020-03-02 10:55:59 +0100

    Add luke12 implementation

---
+++ luke12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var used7IterationsCounter = 0;
+        for (var i = 1000; i <= 9999; i++)
+        {
+            if (i % 1111 == 0)
+            {
+                continue;
+            }
+
+
+            var iterationsCounter = 0;
+            var currentNumber = i;
+            while (true)
+            {
+                iterationsCounter++;
+                currentNumber = SubtractSmallestSortedRepresentation(currentNumber);
+
+                if (currentNumber == 6174)
+                {
+                    if (iterationsCounter == 7)
+                    {
+                        used7IterationsCounter++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine($"Result: {used7IterationsCounter}");
+    }
+
+    static int SubtractSmallestSortedRepresentation(int number)
+    {
+        int ascending = int.Parse(string.Concat(number.ToString("0000").OrderBy(c => c)));
+        int descending = int.Parse(string.Concat(number.ToString("0000").OrderByDescending(c => c)));
+
+        return ascending > descending
+            ? ascending - descending
+            : descending - ascending;
+    }
+}
