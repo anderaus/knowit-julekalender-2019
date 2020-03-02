fb4c9e1f2d07fadaab31500d8e14ff66f110f340
Anders Austad <anders.austad@novanet.no>
2020-03-02 19:57:19 +0100

    Add luke19 implementation

---
+++ luke19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch watch = Stopwatch.StartNew();
+
+        var sum =
+            Enumerable
+                .Range(1, 123454321)
+                .AsParallel()
+                .Where(x => IsHiddenPalindrome(x) && !IsPalindrome(x))
+                .Sum(Convert.ToInt64);
+
+        Console.WriteLine($"Sum: {sum}, time: {watch.Elapsed.TotalSeconds}");
+    }
+
+    static bool IsHiddenPalindrome(long number)
+        => IsPalindrome(number + Convert.ToInt64(string.Join(string.Empty, number.ToString().Reverse())));
+
+    static bool IsPalindrome(long number)
+        => number.ToString().SequenceEqual(number.ToString().Reverse());
+}
