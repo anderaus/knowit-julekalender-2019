ed7563dda92eb1a79abcabd05656872a791ad0c5
Anders Austad <anders.austad@novanet.no>
2020-03-06 16:34:24 +0100

    Add luke22 implementation

---
+++ src/luke22/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var forestLines = File.ReadAllLines("forest.txt");
+        var forest = new bool[forestLines.First().Length, forestLines.Length];
+
+        int x = 0;
+        int y = 0;
+        foreach (var forestLine in forestLines)
+        {
+            foreach (var forestPiece in forestLine)
+            {
+                forest[x++, y] = forestPiece == '#';
+
+            }
+            x = 0;
+            y++;
+        }
+
+        double sumOfHeights = 0;
+
+        for (int x2 = 0; x2 < forest.GetLength(0); x2++)
+        {
+            // Hvis er stamme
+            if (forest[x2, 34])
+            {
+                Console.WriteLine("Found stamme at (x,y) = (" + x2 + "," + 34 + ")");
+                var y2 = 34;
+                while (y2 >= 0 && forest[x2, y2])
+                {
+                    sumOfHeights++;
+                    y2--;
+                }
+                Console.WriteLine("SumOfHeights: " + sumOfHeights);
+            }
+        }
+
+        Console.WriteLine("Result (40 kr per height unit): " + sumOfHeights * 40);
+    }
+}
