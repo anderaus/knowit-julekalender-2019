dd9871d307d0a5ed60cc4cbec114d54e65f5545d
Anders Austad <anders.austad@novanet.no>
2020-03-08 19:42:17 +0100

    Add luke24 implementation

---
+++ src/luke24/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var offset = 0;
+        using (var image = new Image<Rgba32>(4800, 400))
+        {
+            foreach (var line in File.ReadAllLines("turer.txt"))
+            {
+                if (line == "---") offset += 110;
+                else
+                    image[
+                        offset + Convert.ToInt32(line.Split(',')[0]),
+                        380 - Convert.ToInt32(line.Split(',')[1])] = Rgba32.Red;
+            }
+
+            image.Save("result.png");
+        }
+    }
+}
